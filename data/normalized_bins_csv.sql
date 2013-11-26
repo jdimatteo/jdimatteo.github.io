f87e1058e530a91fa7cf9212c9c9662c6aa2a020
John DiMatteo <jdimatteo@gmail.com>
2013-11-26 08:01:09 -0500

    Added plots for all chromosomes

--- data/normalized_bins_csv.sql
+++ data/normalized_bins_csv.sql
@@ -1,6 +1,7 @@
-SELECT cell_type, chromosome, bin, count_fraction, percentile_in_cell_type 
+SELECT cell_type, bin, count_fraction, percentile_in_cell_type 
 FROM normalized_bins 
 WHERE counter_version = 203 AND chromosome = 'chr1'
+ORDER BY cell_type, bin
 INTO OUTFILE '/Users/jdimatteo/DanaFarber/jdimatteo.github.io/data/normalized_bins_chr1.csv'
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
